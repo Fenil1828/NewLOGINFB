a12ffabd89955a439d55a67bede06de9ecd9e77d
Fenil1828 <jasanifenil18@gmail.com>
2025-06-02 12:29:56 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -145,7 +145,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
-import { handleError, handleSuccess } from '../utils';
+import { handleError, handleSuccess } from '../util';
 
 function Signup() {
 
